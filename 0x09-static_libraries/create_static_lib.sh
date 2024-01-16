4b528f95864d4f8a791e233165db51206a4b6fd0
mechatron-hola <holaade1908@gmail.com>
2024-01-16 15:41:01 -0800

    static_libraries in c-lang updated

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcc -c ./*.c && ar rc liball.a ./*.o
+
+# Compile all C files in the current directory into object files
+gcc -c ./*.c 
+
+# Create the static library liball.a from the object files
+ar rc liball.a ./*.o
