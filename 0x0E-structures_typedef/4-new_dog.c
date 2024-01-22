7ef160a20758db1d8d195a8099eb9f6d06f8f056
mechatron-hola <holaade1908@gmail.com>
2024-01-22 23:25:37 +0100

    structures_typedef in c-lang

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,79 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *d_name, *d_owner;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL || age < 0)
+		exit(EXIT_FAILURE);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		exit(EXIT_FAILURE);
+
+	d_name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (d_name == NULL)
+	{
+		free(dog);
+		exit(EXIT_FAILURE);
+	}
+
+	d_owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (d_owner == NULL)
+	{
+		free(d_name);
+		free(dog);
+		exit(EXIT_FAILURE);
+	}
+
+	d_name = _strcpy(d_name, name), d_owner = _strcpy(d_owner, owner);
+
+	dog->name = d_name;
+	dog->owner = d_owner;
+	dog->age = age;
+
+	return (dog);
+}
+
+/**
+ * _strcpy - copies a string into another
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+		dest[i] = src[i];
+
+	dest[i] = 0;
+
+	return (dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+
+	return (len);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: age of dog
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+/**
+ * dog_t - type definition for struct dog
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
